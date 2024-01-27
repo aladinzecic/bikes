a815f129ec0b9cb953e6afa8bb0cabc726ff065e
aladinzecic <aladinzecic10@gmail.com>
2024-01-27 16:24:33 +0100

    deploy

--- allPages/Map/Map.js
+++ allPages/Map/Map.js
@@ -11,7 +11,9 @@ L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{
     minZoom: 0,
     maxZoom: 20,
     attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    ext: 'png'
+    ext: 'png',
+    id: 'mapbox-outdoors',
+    accessToken:'sk.eyJ1IjoiaXFhcmNobGkiLCJhIjoiY2pqZTN1OTNxMGNubDNwbXNhbDNka2J4ZiJ9.UKoT12YRTSSUEnIGHyiScQ'
 }).addTo(map);
 
 let locations;
